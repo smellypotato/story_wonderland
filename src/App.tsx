af93042fe4f6ae61e06e65cf59e110849db275b3
SmellyPotato <hksahenry@yahoo.com.hk>
2022-03-30 02:05:44 +0800

    main

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { Backdrop } from './component/Backdrop';
+import { Footer } from './component/Footer';
+import { Header } from './component/Header';
+import { Home } from './page/Home';
+import { Library } from './page/Library';
+import { Problem } from './page/Problem';
+import { Solution } from './page/Solution';
+import { Story } from './page/Story';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export const App = () => {
+    return (
+        <div className="App">
+            <Header />
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={ <Home /> }/>
+                    <Route path="/story" element={ <Story /> }/>
+                    <Route path="/problem" element={ <Problem /> }/>
+                    <Route path="/solution" element={ <Solution /> }/>
+                    <Route path="/library" element={ <Library /> }/>
+                </Routes>
+                <Footer />
+                <Backdrop />
+            </BrowserRouter>
+        </div>
+    );
 }
-
-export default App;
